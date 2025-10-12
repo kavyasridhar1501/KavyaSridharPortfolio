d4faf77e417c655074744f77ece5a7fa2323fd07
Kavya Sridhar <76093539+kavyasridhar1501@users.noreply.github.com>
2025-10-11 20:53:23 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: isProd ? "/KavyaSridharPortfolio" : undefined,
   assetPrefix: isProd ? "/KavyaSridharPortfolio/" : undefined,
 };
+
 export default nextConfig;
